6d6d77aea71b140be597cfe240a708368cbfc63f
dz_paji <zhangqian2002@outlook.com>
2023-11-06 11:16:53 +0000

    proto-41 development

--- proto41_handler.py
+++ proto41_handler.py
@@ -1,11 +1,68 @@
 import struct
+import socket
+import logging
+import ipaddress
+import configparser
 
 class Proto41Handler:
-    def register(self, client):
-        client.register_handler(0x41, self.handle_proto41)
+    __endpoints = []
+    handler_type = "proto41"
+    handle_protocol = 41
+    __config = configparser.ConfigParser()
+    def __init__(self):
+        self.__config.read('config/app.ini')
+    
+    def register(self, ip):
+        self.__endpoints.append(ip)
         
     def validate(self, packet):
-        return True
+        header = struct.unpack('!BBHHHBBH4s4s', packet[:20])
+        checksum = header[7]
+        src = header[8]
+        if self.__endpoints.__contains__(src):
+            return True
+        
+        return False
     
     def decapsulate(self, packet):
-        pass
+        header = struct.unpack('!BBHHHBBH4s4s', packet[:20])
+        src = header[8]
+        dst = header[9]
+        
+        ipv6_packet = packet[20:]
+        ipv6_header = struct.unpack('!IHBB16s16s', ipv6_packet[:40])
+        next_header = ipv6_header[3]
+        payload_length = ipv6_header[1] #　for reassembling fragmented packets
+        src_ipv6 = ipv6_header[4]
+        dst_ipv6 = ipv6_header[5]
+            
+        # discard the packet if the source address is invalid
+        invalid_addresses = ["FF00::/8", "::1", "::FFFF:0:0/96"]
+        rfc3513_addresses = "::/96"
+        for address in invalid_addresses:
+            if ipaddress.ip_address(src_ipv6).subnet_of(ipaddress.ip_network(address)):
+                logging.error("Discard the packet as it has an invalid source address")
+                return None        
+        
+        if ipaddress.ip_address(src_ipv6).subnet_of(ipaddress.ip_network(rfc3513_addresses)):
+            if not ipaddress.ip_address(src_ipv6).subnet_of(ipaddress.ip_network("::/128")):
+                logging.error("Discard the packet as its src address is a RFC3513 address")
+                
+        # handles next header
+
+        
+        ipv6_header[4] = self.__config['Interface']['IPv6_Address']
+        
+        # need to use a soft state table to keeping track of the packets
+        
+            
+        # recalculating length as IPv4 header might be padded
+        new_payload_length = len(ipv6_packet) + 40 
+        ipv6_header[1] = new_payload_length
+        ipv6_header_raw = struct.pack('!IHBB16s16s', ipv6_header[0], ipv6_header[1], ipv6_header[2], ipv6_header[3], ipv6_header[4], ipv6_header[5])
+        
+        packet = ipv6_header_raw + ipv6_packet
+        return packet
+        
+if __name__ == '__main__':
+    print(128 * '1')
--- socket_interface.py
+++ socket_interface.py
@@ -4,10 +4,13 @@ import configparser
 import logging
 import time
 
+from proto41_handler import Proto41Handler
+
 class SocketInterface:
     __protocols = []
     __config = configparser.ConfigParser()
     __logger = logging.getLogger("sniffer")
+    __handler = None
     
     def __init__(self):
         logging.basicConfig(level=logging.NOTSET)
@@ -15,9 +18,14 @@ class SocketInterface:
         # create a raw socket
         self.raw_socket = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x0800))
         # self.raw_socket.bind(('enp0s3', 0))
-        self.add_protocol()
+        
+        # self.add_protocol() deprecated by add_handler()
 
     def listen(self):
+        if self.__handler == None:
+            logging.error("No handler added")
+            return -1
+        
         while True:
             packet = self.raw_socket.recvfrom(65535)
             raw_header = packet[0][14:34]
@@ -26,28 +34,42 @@ class SocketInterface:
             version = hlv >> 4
             protocol = header[6]
             if version == 4:
-                if self.__protocols.__contains__(protocol):
+                if self.__handler.handle_protocol == protocol:
                     print("IPv4 packet with protocol " + str(protocol) + " received from " + socket.inet_ntoa(header[9]) + " at " + time.strftime("%H:%M:%S", time.localtime()))
+                    if self.__handler.validate(packet):
+                        print("Validated")
+                        ipv6_packet = self.__handler.decapsulate(packet)
+                        
+                        if ipv6_packet != None:
+                            # silently discard the packet
+                            self.inject(ipv6_packet)
                 
-    def add_protocol(self):
-        i = 0
-        self.__config.read('config/app.ini')
-        if self.__config['6in4']['Enabled'] == "True":
-                self.__protocols.append(41)
-                i + 1
-                self.__logger.debug("proto-41 protocol added")
+    # def add_protocol(self):
+    # ''' deprecated by add_handler()
+    # '''
+    #     i = 0
+    #     self.__config.read('config/app.ini')
+    #     if self.__config['6in4']['Enabled'] == "True":
+    #             self.__protocols.append(41)
+    #             i + 1
+    #             self.__logger.debug("proto-41 protocol added")
                 
-        # for testing purposes
-        # self.__protocols.append(6)
+    #     # for testing purposes
+    #     # self.__protocols.append(6)
         
-        self.__logger.info("protocols added: %s", self.__protocols)
-        return i
+    #     self.__logger.info("protocols added: %s", self.__protocols)
+    #     return i
+    
+    def add_handler(self, handler):
+        self.__handler = handler
+        self.__logger.info("%s handler added", handler.handler_type)
         
     def inject(self, packet):        
         self.raw_socket.send(packet)
-                
+        logging.info("Packet injected")                
 
 
 if __name__ == '__main__':
-    sniffer = PacketSniffer()
-    sniffer.listen()
+    socks = SocketInterface()
+    proto41_handler = Proto41Handler()
+    socks.add_handler(proto41_handler)
